6b9c451bc353ea8c7c53fd13faf3822e81421ed6
ddmuzyk <domino2k1@gmail.com>
2022-12-17 17:03:13 +0100

    Adding files

---
+++ script.js
@@ -0,0 +1,49 @@
+let button = document.getElementById('enter');
+let input = document.getElementById('userinput');
+let ul = document.querySelector('ul');
+
+const inputLength = () => {
+    return input.value.length;
+}
+
+const addListAfterClick = (() => {
+    if (inputLength() > 0) {
+        createListElement();
+    } 
+});
+
+const addListAfterKeypress = ((event) => {
+    if (inputLength() > 0 && event.which === 13) {
+        createListElement();
+    } 
+})
+
+const createButton = (() => {
+    let buttonDelete = document.createElement('button');
+    buttonDelete.textContent = "Delete";
+    buttonDelete.classList.add("delete");
+    buttonDelete.onclick = function() {
+        buttonDelete.parentElement.remove();
+    }
+    return buttonDelete;
+})
+
+const createListElement = (() => {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = '';
+    li.appendChild(createButton());
+});
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+// -------------------------------Exercise part----------------------------------
+
+
+
+ul.onclick = function(event) {
+    event.target.classList.toggle('done');
+}
+
